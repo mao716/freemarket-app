466afb77002de65785e73f73dd7c79781132c462
hamaguchi mao <kupokupo44@gmail.com>
2025-10-06 16:56:54 +0900

    Fortify設定完了（メール認証は応用時にON予定）

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,37 +2,38 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail; （メール認証を使う時に追加）
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+// class User extends Authenticatable implements MustVerifyEmail （メール認証を使う時に書き換え）
 class User extends Authenticatable
 {
 	use HasFactory, Notifiable;
 
 	public function profile()
-	{  // 1対1（hasOne）
+	{  // 1対1（hasOne：1ユーザーに1プロフィール）
 		return $this->hasOne(Profile::class);
 	}
 
 	public function items()
-	{    // 1対多（hasMany）
+	{  // 1対多（hasMany：1ユーザーが複数商品を出品）
 		return $this->hasMany(Item::class);
 	}
 
 	public function likes()
-	{
+	{  // 1対多（hasMany：1ユーザーが複数いいね）
 		return $this->hasMany(Like::class);
 	}
 
 	public function comments()
-	{
+	{  // 1対多（hasMany：1ユーザーが複数コメント）
 		return $this->hasMany(Comment::class);
 	}
 
 	public function orders()
-	{   // 自分が購入した注文
+	{   // 1対多（hasMany：1ユーザーが複数注文を持つ）
 		return $this->hasMany(Order::class);
 	}
 }
