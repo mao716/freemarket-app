c5c0a5c401cd21476c8fdee2cf92285446527671
hamaguchi mao <kupokupo44@gmail.com>
2025-11-18 10:58:40 +0900

    style: プロフィール画像のプレースホルダー修正

--- src/resources/views/mypage/profile.blade.php
+++ src/resources/views/mypage/profile.blade.php
@@ -13,7 +13,7 @@
 		<section class="page-section">
 			<div class="comment-head profile-head">
 				<img class="avatar"
-					src="{{ $user->avatar_path ? Storage::url($user->avatar_path) : asset('images/avatar-placeholder.png') }}"
+					src="{{ $user->avatar_path ? Storage::url($user->avatar_path) : asset('images/image-placeholder.png') }}"
 					alt="{{ $user->name }}のアイコン">
 				<div class="comment-author">{{ $user->name }}</div>
 				<a class="button button-outline profile-edit" href="{{ route('mypage.edit') }}">
@@ -21,7 +21,6 @@
 				</a>
 			</div>
 
-			{{-- タブ（出品 / 購入） --}}
 			<nav class="page-tabs" aria-label="表示切替（タブ）">
 				<a class="page-tab {{ $tab === 'sell' ? 'is-active' : '' }}"
 					href="{{ route('mypage.profile', ['page' => 'sell']) }}">
@@ -34,7 +33,6 @@
 			</nav>
 		</section>
 
-		{{-- ====== リスト本体 ====== --}}
 		<section class="page-section items">
 			@if ($tab === 'buy')
 			@if ($purchasedItems->isEmpty())
